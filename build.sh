d63291d78e9226f87e4a13bcb9b71f558d01baa8
Mousticke <akim.benchiha@gmail.com>
2018-05-22 20:21:17 +0200

    Création du build

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+param=$1;
+
+do_commit(){
+	git add .
+	echo "Message du commit"
+	read commitMessage
+	git commit -m "${commitMessage}"
+
+}
+
+do_make(){
+	make all
+}
+
+do_push(){
+	git push -u origin master
+}
+
+do_Make_cm(){
+	make all
+	mv *.o
+	mv main
+	git add .
+	echo "Message du commit"
+	read commitMessage
+	git commit -m $commitMessage
+}
+
+if  [[ $1 = "-cm" ]]; then
+    do_Make_cm
+elif [[ $1 = "-p" ]]; then
+    do_push
+elif [[ $1 = "-make" ]]; then
+	do_make
+elif [[ $1 = "-c" ]]; then
+	do_commit
+fi
